bc8d59c02e5f6e01c34aaf734fd62a8e19ee3768
GuilhermeW3ber <brococaxi@hotmai.com>
2023-06-08 16:41:48 -0300

    1.0.4

---
+++ js/slowLoading.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset:true,
+    distance:'60px',
+    duration:2500,
+    delay:400
+});
+
+ScrollReveal().reveal('.main-title', {delay:500, origin:'left'});
+ScrollReveal().reveal('.navbar-bottom a', '.social img', {delay:500, origin:'bottom', interval:200});
